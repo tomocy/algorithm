7f6e6e2de5d72030accbb448855e21c430aeed1c
tomocy <tomocy.dev@gmail.com>
2019-09-04 01:25:15 +0900

    add solution of selection sort

---
+++ selection_sort/main_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/tomocy/algorithm"
+)
+
+func TestSolve(t *testing.T) {
+	tests := []struct {
+		input, expected []int
+	}{
+		{[]int{5, 3, 6, 2, 1}, []int{1, 2, 3, 5, 6}},
+		{[]int{}, []int{}},
+	}
+
+	for _, test := range tests {
+		actual := solve(test.input)
+		if len(actual) != len(test.expected) {
+			t.Fatalf("unexpected length: got %d, expect %d\n", len(actual), len(test.expected))
+		}
+		for i, expected := range test.expected {
+			if actual[i] != expected {
+				algorithm.Reportln(t, "selection sort", actual[i], expected)
+			}
+		}
+	}
+}
