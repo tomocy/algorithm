02517c073363057a2f1b3a796ac27579dadf9157
tomocy <tomocy.dev@gmail.com>
2019-09-04 20:48:17 +0900

    add solution2 of length

--- length/main.go
+++ length/main.go
@@ -5,3 +5,13 @@ func main() {}
 func solve(vs []int) int {
 	return len(vs)
 }
+
+func solve2(vs []int) (l int) {
+	defer func() {
+		if err := recover(); err != nil {
+			l = 0
+		}
+	}()
+
+	return 1 + solve(vs[1:])
+}
