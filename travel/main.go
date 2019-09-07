7e19dcfa267f8fa4b40ca1f267c496750fc752a0
tomocy <tomocy.dev@gmail.com>
2019-09-08 02:21:47 +0900

    add dynamic of travel

---
+++ travel/main.go
@@ -0,0 +1,88 @@
+package main
+
+func main() {}
+
+func dynamic(items map[string]tour, limit float64) []string {
+	grid := make(grid)
+	names, base := names(items), calcCostBase(items)
+	var lastName string
+	for _, name := range names {
+		item := items[name]
+		for cost := base; cost <= limit; cost += base {
+			var maxCell *cell
+			if item.cost <= cost {
+				maxCell = &cell{
+					val: item.val, names: []string{name},
+				}
+				if cell := grid.cell(lastName, cost-item.cost); cell != nil {
+					maxCell.val += cell.val
+					maxCell.names = append(maxCell.names, cell.names...)
+				}
+			}
+			if cell := grid.cell(lastName, cost); cell != nil && (maxCell == nil || maxCell.val < cell.val) {
+				maxCell = cell
+			}
+
+			grid.setCell(name, cost, maxCell)
+		}
+
+		lastName = name
+	}
+
+	return grid.cell(lastName, limit).names
+}
+
+func names(items map[string]tour) []string {
+	ns := make([]string, len(items))
+	var i int
+	for n := range items {
+		ns[i] = n
+		i++
+	}
+
+	return ns
+}
+
+func calcCostBase(items map[string]tour) float64 {
+	base := 1.0
+	for _, item := range items {
+		dec := item.cost - float64(int(item.cost))
+		if dec == 0 {
+			continue
+		}
+
+		if dec < base {
+			base = dec
+		}
+	}
+
+	return base
+}
+
+type grid map[string]map[float64]*cell
+
+func (g grid) cell(i string, j float64) *cell {
+	row := g[i]
+	if row == nil {
+		return nil
+	}
+
+	return row[j]
+}
+
+func (g *grid) setCell(i string, j float64, c *cell) {
+	if (*g)[i] == nil {
+		(*g)[i] = make(map[float64]*cell)
+	}
+
+	(*g)[i][j] = c
+}
+
+type cell struct {
+	val   float64
+	names []string
+}
+
+type tour struct {
+	val, cost float64
+}
