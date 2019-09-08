3c00c1b13752c5bfa10c3d620b71f5449d624cd3
tomocy <tomocy.dev@gmail.com>
2019-09-09 06:19:07 +0900

    add dynamic for longest common subsequence

--- longest_common_subsequence/main.go
+++ longest_common_subsequence/main.go
@@ -2,4 +2,61 @@ package main
 
 func main() {}
 
-func solve() {}
+func dynamic(vocabs []string, s string) string {
+	var longest string
+	var max int
+	for _, vocab := range vocabs {
+		guessed := countCommonSubseqence(vocab, s)
+		if max < guessed {
+			longest, max = vocab, guessed
+		}
+	}
+
+	return longest
+}
+
+func countCommonSubseqence(a, b string) int {
+	grid := newGrid(len(a), len(b))
+	for i, p := range a {
+		for j, q := range b {
+			if i == j && p == q {
+				grid[i][j] = grid.cell(i-1, j-1) + 1
+				continue
+			}
+
+			max := grid.cell(i-1, j)
+			if guessed := grid.cell(i, j-1); max < guessed {
+				max = guessed
+			}
+
+			grid[i][j] = max
+		}
+	}
+
+	return grid.cell(len(a)-1, len(b)-1)
+}
+
+func newGrid(xLen, yLen int) grid {
+	grid := make(grid, xLen)
+	for i := range grid {
+		grid[i] = make([]int, yLen)
+	}
+
+	return grid
+}
+
+type grid [][]int
+
+func (g grid) cell(i, j int) int {
+	if i < 0 || len(g) <= i {
+		return 0
+	}
+	if len(g) < 1 {
+		return 0
+	}
+	if j < 0 || len(g[0]) <= j {
+		return 0
+	}
+
+	return g[i][j]
+}
