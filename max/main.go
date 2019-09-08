c2830aefc1361818b6b8e617e6a3c71c566966bb
tomocy <tomocy.dev@gmail.com>
2019-09-09 01:44:40 +0900

    add recursive for max

--- max/main.go
+++ max/main.go
@@ -12,3 +12,20 @@ func simple(items []int) int {
 
 	return max
 }
+
+func recursive(items []int) int {
+	switch len(items) {
+	case 0:
+		return 0
+	case 1:
+		return items[0]
+	default:
+		max := items[0]
+		guessed := recursive(items[1:])
+		if max < guessed {
+			max = guessed
+		}
+
+		return max
+	}
+}
