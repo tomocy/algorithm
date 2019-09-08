22fbb4f41a69f73145d21f5022e8ec9c72cc5610
tomocy <tomocy.dev@gmail.com>
2019-09-08 21:54:34 +0900

    add length

---
+++ length/main_test.go
@@ -0,0 +1,36 @@
+package main
+
+import "testing"
+
+var solutions = map[string]func([]interface{}) int{}
+
+func TestSolve(t *testing.T) {
+	tests := []struct {
+		inputs   []interface{}
+		expected int
+	}{
+		{[]interface{}{1, "1", true}, 3},
+		{[]interface{}{}, 0},
+	}
+
+	for name, s := range solutions {
+		t.Run(name, func(t *testing.T) {
+			for _, test := range tests {
+				actual := s(test.inputs)
+				if actual != test.expected {
+					t.Errorf("got %d, expect %d\n", actual, test.expected)
+				}
+			}
+		})
+	}
+}
+
+func BenchmarkSolve(b *testing.B) {
+	for name, s := range solutions {
+		b.Run(name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				s([]interface{}{1, "1", true})
+			}
+		})
+	}
+}
