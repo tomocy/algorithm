42f18989b7a0ce667ad25ef108a82bcffcd1770f
tomocy <tomocy.dev@gmail.com>
2019-09-08 20:34:09 +0900

    fix

--- search/shortest_path/main_test.go
+++ search/shortest_path/main_test.go
@@ -34,14 +34,30 @@ func TestSolve(t *testing.T) {
 		},
 	}
 
-	for _, test := range tests {
-		for name, s := range solutions {
-			t.Run(name, func(t *testing.T) {
+	for name, s := range solutions {
+		t.Run(name, func(t *testing.T) {
+			for _, test := range tests {
 				actual := s(test.input.graph, test.input.start)
 				if actual != test.expected {
 					algorithm.Reportln(t, "shortest path", actual, test.expected)
 				}
-			})
-		}
+			}
+		})
+	}
+}
+
+func BenchmarkSolve(b *testing.B) {
+	for name, s := range solutions {
+		b.Run(name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				s(map[string][]string{
+					"you":    []string{"alice", "bob", "claire"},
+					"bob":    []string{"anuj", "peggy"},
+					"alice":  []string{"peggy"},
+					"claire": []string{"thom", "jonny"},
+					"anuj":   []string{}, "peggy": []string{}, "thom": []string{}, "jonny": []string{},
+				}, "you")
+			}
+		})
 	}
 }
