35bf61fb99ec6d4df04ae2eb8bcd53a0ecdd6ce6
tomocy <tomocy.dev@gmail.com>
2019-09-08 21:48:14 +0900

    add factorial

---
+++ factorial/main_test.go
@@ -0,0 +1,36 @@
+package main
+
+import "testing"
+
+var solutions = map[string]func(int) int{}
+
+func TestSolve(t *testing.T) {
+	tests := []struct {
+		input, expected int
+	}{
+		{5, 120},
+		{1, 1},
+		{0, 1},
+	}
+
+	for name, s := range solutions {
+		t.Run(name, func(t *testing.T) {
+			for _, test := range tests {
+				actual := s(test.input)
+				if actual != test.expected {
+					t.Errorf("got %d, expect %d\n", actual, test.expected)
+				}
+			}
+		})
+	}
+}
+
+func BenchmarkSolve(b *testing.B) {
+	for name, s := range solutions {
+		b.Run(name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				s(5)
+			}
+		})
+	}
+}
