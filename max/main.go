bffb8c48e054137818d6a2868a7ed244cff64fec
tomocy <tomocy.dev@gmail.com>
2019-09-08 22:00:06 +0900

    add simple for max

--- max/main.go
+++ max/main.go
@@ -2,4 +2,13 @@ package main
 
 func main() {}
 
-func solve() {}
+func simple(items []int) int {
+	var max int
+	for i, item := range items {
+		if i == 0 || max < item {
+			max = item
+		}
+	}
+
+	return max
+}
