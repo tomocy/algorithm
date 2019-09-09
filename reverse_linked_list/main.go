bdf3fd74f149f46fec22276dfe68898324584907
tomocy <tomocy.dev@gmail.com>
2019-09-09 22:50:17 +0900

    add with stack for reverse linked list

--- reverse_linked_list/main.go
+++ reverse_linked_list/main.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {}
 
 func simple(head *node) *node {
@@ -14,7 +18,53 @@ func simple(head *node) *node {
 	return prev
 }
 
+func withStack(head *node) *node {
+	var stack stack
+	for n := head; n != nil; n = n.next {
+		stack.push(n.val)
+	}
+	if len(stack) < 1 {
+		return nil
+	}
+
+	popped, _ := stack.pop()
+	reversed := &node{
+		val: popped,
+	}
+	current := reversed
+	for v, ok := stack.pop(); ok; v, ok = stack.pop() {
+		current.next = &node{
+			val: v,
+		}
+		current = current.next
+	}
+
+	return reversed
+}
+
+type stack []int
+
+func (s *stack) push(a int) {
+	*s = append(*s, a)
+}
+
+func (s *stack) pop() (int, bool) {
+	if len(*s) < 1 {
+		return 0, false
+	}
+
+	i := len(*s) - 1
+	last := (*s)[i]
+	*s = (*s)[:i]
+
+	return last, true
+}
+
 type node struct {
 	val  int
 	next *node
+}
+
+func (n node) String() string {
+	return fmt.Sprintf("%d->%s", n.val, n.next)
 }
