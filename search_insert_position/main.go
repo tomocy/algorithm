482c5fe33518dcd7135c4b12f5fe680a7455c438
tomocy <tomocy.dev@gmail.com>
2019-09-12 20:41:45 +0900

    add with binary search for search insert position

--- search_insert_position/main.go
+++ search_insert_position/main.go
@@ -2,4 +2,28 @@ package main
 
 func main() {}
 
-func solve() {}
+func binarySearch(vs []int, x int) int {
+	begin, end := 0, len(vs)-1
+	for begin <= end {
+		mid := (begin + end) / 2
+		guessed := vs[mid]
+		if guessed == x {
+			return mid
+		}
+		if x < guessed {
+			end = mid - 1
+		} else {
+			begin = mid + 1
+		}
+	}
+
+	lastI := -1
+	for i, v := range vs {
+		if x < v {
+			break
+		}
+		lastI = i
+	}
+
+	return lastI + 1
+}
