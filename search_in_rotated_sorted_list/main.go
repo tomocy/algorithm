5d048e12309d18505ee1d21e07ca959e01532e39
tomocy <tomocy.dev@gmail.com>
2019-09-13 03:59:15 +0900

    flush search in rorated sorted list

--- search_in_rotated_sorted_list/main.go
+++ search_in_rotated_sorted_list/main.go
@@ -3,37 +3,9 @@ package main
 func main() {}
 
 func simple(vs []int, x int) int {
-	for i, v := range vs {
-		if v == x {
-			return i
-		}
-	}
-
 	return -1
 }
 
 func binarySearch(vs []int, x int) int {
-	begin, end := 0, len(vs)-1
-	for begin <= end {
-		mid := (begin + end) / 2
-		if vs[mid] == x {
-			return mid
-		}
-
-		if vs[begin] <= vs[mid] {
-			if vs[begin] <= x && x < vs[mid] {
-				end = mid - 1
-			} else {
-				begin = mid + 1
-			}
-		} else {
-			if vs[mid] < x && x <= vs[end] {
-				begin = mid + 1
-			} else {
-				end = mid - 1
-			}
-		}
-	}
-
 	return -1
 }
