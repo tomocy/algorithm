9939e4e72fbb96e42356d9dc22d6f118aae0f73d
tomocy <tomocy.dev@gmail.com>
2019-09-13 03:55:18 +0900

    review binary search for find min in rorated sorated list

--- find_minimum_in_rotated_sorted_list/main.go
+++ find_minimum_in_rotated_sorted_list/main.go
@@ -16,5 +16,43 @@ func simple(vs []int) int {
 }
 
 func binarySearch(vs []int) int {
+	begin, end := 0, len(vs)-1
+	if vs[begin] < vs[end] {
+		return vs[begin]
+	}
+
+	for begin <= end {
+		mid := (begin + end) / 2
+		left, right := mid-1, mid+1
+
+		if left < begin {
+			min := vs[mid]
+			if right <= end && vs[right] < min {
+				min = vs[right]
+			}
+			return min
+		}
+		if end < right {
+			min := vs[mid]
+			if begin <= left && vs[left] < min {
+				min = vs[left]
+			}
+			return min
+		}
+
+		if vs[mid] < vs[left] {
+			return vs[mid]
+		}
+		if vs[right] < vs[mid] {
+			return vs[right]
+		}
+
+		if vs[begin] <= vs[mid] {
+			begin = mid + 1
+		} else {
+			end = mid - 1
+		}
+	}
+
 	return -1
 }
