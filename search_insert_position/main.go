a3a2957c08729f0ecaccd79c3aabb51a82d051ad
tomocy <tomocy.dev@gmail.com>
2019-09-13 03:27:29 +0900

    review search insert position

--- search_insert_position/main.go
+++ search_insert_position/main.go
@@ -3,5 +3,27 @@ package main
 func main() {}
 
 func binarySearch(vs []int, x int) int {
-	return -1
+	begin, end := 0, len(vs)-1
+	for begin <= end {
+		mid := (begin + end) / 2
+		if vs[mid] == x {
+			return mid
+		}
+		if vs[mid] < x {
+			begin = mid + 1
+		} else {
+			end = mid - 1
+		}
+	}
+
+	lastI := -1
+	for i, v := range vs {
+		if x < v {
+			break
+		}
+
+		lastI = i
+	}
+
+	return lastI + 1
 }
