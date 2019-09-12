5561745f22ef39da8745515f7c36161fded96d83
tomocy <tomocy.dev@gmail.com>
2019-09-13 03:45:56 +0900

    review simple for find min in rotated sorted list

--- find_minimum_in_rotated_sorted_list/main.go
+++ find_minimum_in_rotated_sorted_list/main.go
@@ -3,7 +3,16 @@ package main
 func main() {}
 
 func simple(vs []int) int {
-	return -1
+	var lastV int
+	for i, v := range vs {
+		if i != 0 && v < lastV {
+			return v
+		}
+
+		lastV = v
+	}
+
+	return vs[0]
 }
 
 func binarySearch(vs []int) int {
