cb69f6ff7066d4e59152fa7fc27aa6dabf77ce3e
tomocy <tomocy.dev@gmail.com>
2019-09-13 03:41:06 +0900

    flush find min in rorate sorted list

--- find_minimum_in_rotated_sorted_list/main.go
+++ find_minimum_in_rotated_sorted_list/main.go
@@ -3,55 +3,9 @@ package main
 func main() {}
 
 func simple(vs []int) int {
-	if len(vs) < 1 {
-		return 0
-	}
-	lastV := vs[0]
-	for _, v := range vs[1:] {
-		if v < lastV {
-			return v
-		}
-	}
-
-	return vs[0]
+	return -1
 }
 
 func binarySearch(vs []int) int {
-	begin, end := 0, len(vs)-1
-	if vs[begin] < vs[end] {
-		return vs[begin]
-	}
-	for begin <= end {
-		mid := (begin + end) / 2
-		left, right := mid-1, mid+1
-		if left < 0 {
-			min := vs[mid]
-			if right < len(vs) && vs[right] < min {
-				min = vs[right]
-			}
-			return min
-		}
-		if len(vs) <= right {
-			min := vs[mid]
-			if 0 <= left && vs[left] < min {
-				min = vs[left]
-			}
-			return min
-		}
-
-		if vs[mid] < vs[left] {
-			return vs[mid]
-		}
-		if vs[right] < vs[mid] {
-			return vs[right]
-		}
-
-		if vs[0] < vs[mid] {
-			begin = mid + 1
-		} else {
-			end = mid - 1
-		}
-	}
-
 	return -1
 }
