7507c2a8a06c8b40680b1c6a0455c67f0390213b
tomocy <tomocy.dev@gmail.com>
2019-09-13 21:30:53 +0900

    add recursion for pow

--- pow/main_test.go
+++ pow/main_test.go
@@ -1,8 +1,13 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-var solutions = map[string]func(float64, int) float64{}
+var solutions = map[string]func(float64, int) float64{
+	"recursion": recursion,
+}
 
 func TestSolve(t *testing.T) {
 	type input struct {
@@ -13,16 +18,19 @@ func TestSolve(t *testing.T) {
 		input    input
 		expected float64
 	}{
+		{input{2, 0}, 1},
+		{input{2, 1}, 2},
 		{input{2, 10}, 1024},
-		{input{2.1, 3}, 9.261},
+		{input{2.1, 3}, 9.261000000000001},
 		{input{2, -2}, 0.25},
+		{input{0.00001, 2147483647}, 0},
 	}
 
 	for name, s := range solutions {
 		t.Run(name, func(t *testing.T) {
 			for _, test := range tests {
 				actual := s(test.input.x, test.input.n)
-				if actual != test.expected {
+				if fmt.Sprint(actual) != fmt.Sprint(test.expected) {
 					t.Errorf("got %F, expect %F", actual, test.expected)
 				}
 			}
