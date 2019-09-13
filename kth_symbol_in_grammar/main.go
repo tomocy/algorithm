6a929c2d774f2659e688be199218ecb0d52f3cbd
tomocy <tomocy.dev@gmail.com>
2019-09-14 05:08:21 +0900

    review recursion for kth symbol in grammar

--- kth_symbol_in_grammar/main.go
+++ kth_symbol_in_grammar/main.go
@@ -3,5 +3,18 @@ package main
 func main() {}
 
 func recursion(n, k int) int {
-	return -1
+	if n == 1 {
+		return 0
+	}
+
+	l := k
+	if k%2 != 0 {
+		l++
+	}
+	original := recursion(n-1, l/2)
+
+	if k%2 != 0 {
+		return original
+	}
+	return (original + 1) % 2
 }
