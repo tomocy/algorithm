3b43544dfd1750ed93237ddc0d780a5b3d77ebb9
tomocy <tomocy.dev@gmail.com>
2019-09-14 05:00:49 +0900

    review binary search for cap to ship pkgs within days

--- capacity_to_ship_packages_within_d_days/main.go
+++ capacity_to_ship_packages_within_d_days/main.go
@@ -3,5 +3,44 @@ package main
 func main() {}
 
 func binarySearch(ps []int, d int) int {
-	return -1
+	max := func(vs []int) int {
+		var max int
+		for i, v := range vs {
+			if i == 0 || max < v {
+				max = v
+			}
+		}
+		return max
+	}
+	sum := func(vs []int) int {
+		var sum int
+		for _, v := range vs {
+			sum += v
+		}
+		return sum
+	}
+
+	begin, end := max(ps), sum(ps)
+	for begin < end {
+		cap := (begin + end) / 2
+		var sum int
+		neededs := 1
+		for _, p := range ps {
+			if cap < sum+p {
+				neededs++
+				sum = p
+				continue
+			}
+
+			sum += p
+		}
+
+		if d < neededs {
+			begin = cap + 1
+		} else {
+			end = cap
+		}
+	}
+
+	return begin
 }
