0c63ff765dd72ad9c5b2a8318080219ad3d493c6
tomocy <tomocy.dev@gmail.com>
2019-09-14 05:03:47 +0900

    review recursion for pow

--- pow/main.go
+++ pow/main.go
@@ -3,5 +3,19 @@ package main
 func main() {}
 
 func recursion(x float64, n int) float64 {
-	return -1
+	if n < 0 {
+		return 1 / recursion(x, -n)
+	}
+	if n == 0 {
+		return 1
+	}
+	if n == 1 {
+		return x
+	}
+
+	r := recursion(x, n/2)
+	if n%2 == 0 {
+		return r * r
+	}
+	return r * r * x
 }
