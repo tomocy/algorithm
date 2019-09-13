44b5461bd94593047b123f459fa7b25e2e44df36
tomocy <tomocy.dev@gmail.com>
2019-09-14 05:01:34 +0900

    flush recursion for pow

--- pow/main.go
+++ pow/main.go
@@ -3,21 +3,5 @@ package main
 func main() {}
 
 func recursion(x float64, n int) float64 {
-	if n < 0 {
-		return 1 / recursion(x, -n)
-	}
-
-	if n == 0 {
-		return 1
-	}
-	if n == 1 {
-		return x
-	}
-
-	r := recursion(x, n/2)
-	if n%2 == 0 {
-		return r * r
-	} else {
-		return r * r * x
-	}
+	return -1
 }
