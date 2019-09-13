f2c0741dc2a401d03e02da605c7b898746727b8d
tomocy <tomocy.dev@gmail.com>
2019-09-14 05:04:48 +0900

    flush recursion for kth symbol in grammar

--- kth_symbol_in_grammar/main.go
+++ kth_symbol_in_grammar/main.go
@@ -3,19 +3,5 @@ package main
 func main() {}
 
 func recursion(n, k int) int {
-	if n == 0 {
-		return 0
-	}
-
-	l := k
-	if k%2 != 0 {
-		l++
-	}
-
-	x := recursion(n-1, l/2)
-	if k%2 != 0 {
-		return x
-	}
-
-	return (x + 1) % 2
+	return -1
 }
