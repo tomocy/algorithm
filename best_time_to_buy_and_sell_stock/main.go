18ade9084f4b4f16c63f1bd308b91d1939b50e67
tomocy <tomocy.dev@gmail.com>
2019-09-15 05:24:15 +0900

    flush max from min for best time to buy and sell stock

--- best_time_to_buy_and_sell_stock/main.go
+++ best_time_to_buy_and_sell_stock/main.go
@@ -48,14 +48,5 @@ func (g *grid) setCell(i, j int, c cell) {
 type cell int
 
 func maxFromMin(ps []int) int {
-	var min, max int
-	for i, p := range ps {
-		if i == 0 || p < min {
-			min = p
-		} else if guessed := p - min; max < guessed {
-			max = guessed
-		}
-	}
-
-	return max
+	return -1
 }
