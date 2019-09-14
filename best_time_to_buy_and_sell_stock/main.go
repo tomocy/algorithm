b7bca8f2ee3f971829ec5f74a13d7217a6f0d68a
tomocy <tomocy.dev@gmail.com>
2019-09-15 05:23:34 +0900

    review dynamic for best time to buy and sell stock

--- best_time_to_buy_and_sell_stock/main.go
+++ best_time_to_buy_and_sell_stock/main.go
@@ -3,8 +3,49 @@ package main
 func main() {}
 
 func dynamic(ps []int) int {
-	return -1
+	grid := make(grid)
+	var lastP, lastQ int
+	for i, p := range ps {
+		lastQ = 0
+		for j, q := range ps {
+			max := grid.cell(p, lastQ)
+			if guessed := grid.cell(lastP, q); max < guessed {
+				max = guessed
+			}
+			if i <= j {
+				if guessed := cell(q - p); max < guessed {
+					max = guessed
+				}
+			}
+
+			grid.setCell(p, q, max)
+			lastQ = q
+		}
+		lastP = p
+	}
+
+	return int(grid.cell(lastP, lastQ))
 }
+
+type grid map[int]map[int]cell
+
+func (g grid) cell(i, j int) cell {
+	if g[i] == nil {
+		return 0
+	}
+
+	return g[i][j]
+}
+
+func (g *grid) setCell(i, j int, c cell) {
+	if (*g)[i] == nil {
+		(*g)[i] = make(map[int]cell)
+	}
+
+	(*g)[i][j] = c
+}
+
+type cell int
 
 func maxFromMin(ps []int) int {
 	var min, max int
