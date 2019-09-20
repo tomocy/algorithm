b21a5861d0d0fd0ed63e90278a294d4005c1df64
tomocy <tomocy.dev@gmail.com>
2019-09-20 22:10:08 +0900

    review recursive for max depth of binary tree

--- max_depth_of_binary_tree/main.go
+++ max_depth_of_binary_tree/main.go
@@ -3,7 +3,16 @@ package main
 func main() {}
 
 func recursive(root *node) int {
-	return -1
+	if root == nil {
+		return 0
+	}
+
+	max := 1 + recursive(root.left)
+	if guessed := 1 + recursive(root.right); max < guessed {
+		max = guessed
+	}
+
+	return max
 }
 
 type node struct {
