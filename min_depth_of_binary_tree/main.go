0d46a779462442e250b8685d65b6c8ce1d6bad6d
tomocy <tomocy.dev@gmail.com>
2019-09-20 22:12:09 +0900

    flush recursive for min depth of binary tree

--- min_depth_of_binary_tree/main.go
+++ min_depth_of_binary_tree/main.go
@@ -3,24 +3,7 @@ package main
 func main() {}
 
 func recursive(root *node) int {
-	if root == nil {
-		return 0
-	}
-
-	switch {
-	case root.left != nil && root.right != nil:
-		min := 1 + recursive(root.left)
-		if guessed := 1 + recursive(root.right); guessed < min {
-			min = guessed
-		}
-		return min
-	case root.left != nil && root.right == nil:
-		return 1 + recursive(root.left)
-	case root.left == nil && root.right != nil:
-		return 1 + recursive(root.right)
-	default:
-		return 1
-	}
+	return -1
 }
 
 type node struct {
