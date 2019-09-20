0554184d157e1455170c43ddf7b07ad07bdafd9b
tomocy <tomocy.dev@gmail.com>
2019-09-20 22:16:49 +0900

    review recursive for min depth of binary tree

--- min_depth_of_binary_tree/main.go
+++ min_depth_of_binary_tree/main.go
@@ -3,7 +3,24 @@ package main
 func main() {}
 
 func recursive(root *node) int {
-	return -1
+	if root == nil {
+		return 0
+	}
+
+	switch {
+	case root.left != nil && root.right != nil:
+		min := 1 + recursive(root.left)
+		if guessed := 1 + recursive(root.right); guessed < min {
+			min = guessed
+		}
+		return min
+	case root.left == nil && root.right != nil:
+		return 1 + recursive(root.right)
+	case root.left != nil && root.right == nil:
+		return 1 + recursive(root.left)
+	default:
+		return 1
+	}
 }
 
 type node struct {
